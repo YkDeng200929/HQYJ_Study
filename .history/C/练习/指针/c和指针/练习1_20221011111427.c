edb0aee403c86d20719cb126d5e9a5d2bb7b3a80
YkDeng200929 <dmtmd520@hotmail.com>
2022-10-11 23:00:12 +0800

    进制与编码

--- .history/C/练习/指针/c和指针/练习1_20221011111427.c
+++
@@ -1,5 +0,0 @@
-/*
-编写函数，进行搜索字符串，查找给定字符集合中出现的字符
-    char *find_char(char const *source, char const)
-
-*/
--- .history/C/练习/指针/c和指针/练习1_20221011111449.c
+++
@@ -1,8 +0,0 @@
-/*
-编写函数，进行搜索字符串，查找给定字符集合中出现的字符
-    char *find_char(char const *source, char const *chars);
-*/
-
-#include <stdio.h>
-
-int main()
