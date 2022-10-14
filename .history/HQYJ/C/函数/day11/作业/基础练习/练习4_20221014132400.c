3e07f63282d3bf62732a1febb931cb1adadcaf81
YkDeng200929 <dmtmd520@hotmail.com>
2022-10-14 13:24:20 +0800

    修改后推送

---
+++ .history/HQYJ/C/函数/day11/作业/基础练习/练习4_20221014132400.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void delete_space(char *buffer)
+{
+    char buf2[1024];:
+    int i=0, j=0;
+    while (buffer[i] != '\0')
+    {
+        if (buffe++r[i] == ' ')
+        {
+            i++;
+            continue;            
+        }
+        else
+        {
+            buf2[j++] = buffer[i++];
+        }
+    }
+    buf2[j] = '\0';
+    printf("%s\n", buf2);
+}
+
+int main()
+{
+    char buf[1024] = "hello world";
+    printf("输入: ");
+    //scanf("%s", buf);
+    printf("%s\n", buf);
+    delete_space(buf);
+    printf("%s\n", buf);
+
+    return 0;
+}
