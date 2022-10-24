3c1e7deeb8c1a500f9aba325743ab35ef92d37f9
YkDeng200929 <dmtmd520@hotmail.com>
2022-10-24 16:13:05 +0800

    递归含义

--- HQYJ/C/函数/递归/练习/1.c
+++ HQYJ/C/函数/递归/练习/1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/*递归:
+已知条件用于退出递归
+未知条件 ( 规律 ) 用于递归
+*/
 // 实现阶乘
 int n(int num)
 {
@@ -8,7 +11,7 @@ int n(int num)
     else
     // 找到已知条件, num = 1 时
         return num * n(num - 1);
-    // 已知:  num = 1 : 用于退出循
+    // 已知:  num = 1 : 用于退出循环
     // 未知: num > 1; num * (num - 1) : 用于递归
 }
 
