e3a7d556ca546a4e9829d7964d4c0ad471e953ab
YkDeng200929 <dmtmd520@hotmail.com>
2022-10-31 16:00:24 +0800

    teach链式队列

--- HQYJ/数据结构/队列/链式队列/teach_linkque.c
+++ HQYJ/数据结构/队列/链式队列/teach_linkque.c
@@ -24,6 +24,7 @@ void my_init(struct linkqueue *q)
     q->rear = NULL;
 }
 
+// 初始化2
 void init_linkqueue(struct linkqueue *lq)
 {
     lq->head.data = 0;
@@ -32,6 +33,7 @@ void init_linkqueue(struct linkqueue *lq)
     return ;
 }
 
+// 进队列
 bool en_linkqueue(struct linkqueue *lq, int val)
 {
     struct node *pnew = (struct node *)malloc(sizeof(struct node));
@@ -41,10 +43,12 @@ bool en_linkqueue(struct linkqueue *lq, int val)
     pnew->next = NULL;
 
     lq->rear->next = pnew;
+    // !!!
+    lq->rear = pnew;
     return true;
 }
 
-// my enque
+// my 进队列
 bool my_enque(struct linkqueue *q)
 {
     struct node *pnew = (struct node *)malloc(sizeof(struct node));
@@ -53,13 +57,15 @@ bool my_enque(struct linkqueue *q)
     
 }
 
-// my dequeue
+// my 出队列
 bool my_deque(struct linkqueue *lq)
 {
     struct node *del, *prev;
-    while (lq->rear->next != lq->front)
+    while (lq->rear != lq->front)
     {
-        for (prev = lq->rear->next, del = prev->next; del->next != lq->front; prev = prev->next)
+        printf("in\n");
+        // 指向有问题
+        for (prev = lq->rear, del = prev->next; del->next != lq->front; prev = prev->next)
         {}
         printf("%d\n", del->data);
         prev->next = lq->front;
@@ -68,14 +74,43 @@ bool my_deque(struct linkqueue *lq)
     printf("All out\n");
 }
 
+// teacher 出队列
+bool de_linkqueue(struct linkqueue *lq, int *pval)
+{
+    // 此时队列已空
+    if (lq->front == lq->rear)
+    return false;
+
+    // [rear] <-- [] <-- [] <--[] <--[head(front)]
+    struct node *ptemp;
+    ptemp = lq->front->next;
+
+    // 如果只有尾节点, 则让队尾重新指向头节点
+    if (lq->front->next == lq->rear)
+    {
+        lq->rear = lq->front;
+    }
+    lq->front->next = lq->front->next->next;
+    *pval =  ptemp->data;
+    ptemp->next = NULL;
+    free(ptemp);
+
+    return true;
+}
+
 int main(void)
 {
+    int val;
     struct linkqueue que;
     init_linkqueue(&que);
     en_linkqueue(&que, 10);
     en_linkqueue(&que, 20);
     en_linkqueue(&que, 30);
-    my_deque(&que);
+    //my_deque(&que);
+    while (de_linkqueue(&que, &val))
+    {
+        printf("%d\n", val);
+    }
 
     return 0;
 }
