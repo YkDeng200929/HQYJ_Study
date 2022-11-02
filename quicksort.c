f4de4cfbdc65cae215d08560c5cd0bb9eb5a90fe
Ykdeng_Win <dmtmd520@hotmail.com>
2022-11-02 20:03:16 +0800

    更改

---
+++ quicksort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void quicksort(int arr[], int first, int last)
+{
+    int low = first;
+    int high = last;
+    int key = arr[first];
+    if (first >= last)
+    return ;
+    while ()
+}
+
+int main(void)
+{
+    return 0;
+}
