e523eb85d251a741624513ef1d55e4afd27ec612
Ykdeng_Win <dmtmd520@hotmail.com>
2022-11-02 09:56:43 +0800

    tree

---
+++ Class/数据结构/二叉树/teach_tree.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct btree
+{
+    int data;
+    struct btree *left;
+    struct btree *right;
+};
+
+int main(void)
+{
+    struct btree
+
+    return 0;
+}
