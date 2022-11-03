57291a93521e5863882d98fc04808edb8fc0d144
Ykdeng_Win <dmtmd520@hotmail.com>
2022-11-03 12:30:53 +0800

    sprintf

--- Class/文件IO/sprintf/sprintf.c
+++ Class/文件IO/sprintf/sprintf.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    char buf[] ="hello";
+    char buf[] ="helloqwdjw";
 
-    sprintf(buf, "Good\n"); // 把字符串覆盖到 buf 对应地址的字符串
+    sprintf(buf, "Good"); // 把字符串覆盖到 buf 对应地址的字符串
     fprintf(stdout, "%s", buf);
 
     return  0;
