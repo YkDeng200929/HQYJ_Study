439573d6c3d147c228d7f64ac2c957dbcd01d3a2
Ykdeng_Win <dmtmd520@hotmail.com>
2023-03-14 17:15:58 +0800

    change-3static.c-成员函数不能通过类名访问

--- Class/11C++/3day/3static.cpp
+++ Class/11C++/3day/3static.cpp
@@ -9,7 +9,7 @@ class A
         {
             cout << "show" << i << endl;
         }
-        static void staticshow()
+        static void staticshow()   // 静态成员函数
         {
             cout << "static show" << i << endl;
         }
@@ -17,7 +17,7 @@ class A
         static int i;
 };
 
-int A::i = 20; // 静态成员的初始化必须写到类外
+int A::i = 20;        // 静态成员的初始化必须写到类外
 
 int main(void)
 {
@@ -26,8 +26,9 @@ int main(void)
     a.i = 80;
     cout << a.i << endl;
     cout << A::i << endl;
-    A::staticshow(); // 只用静态的东西才能直接使用类名加域操作符调用
-    //A::show; 会报错
+    A::staticshow(); // 静态成员函数可以通过<类名>::<静态函数函数名>的方式调用函数
+//  A::show;         // 会报错 vscode下没报错
+                     // error: invalid use of non-static member function ‘void A::show()
 
     return 0;
 }
